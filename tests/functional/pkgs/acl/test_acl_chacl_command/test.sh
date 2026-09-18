ca0d40d298e1573ea0f1ef895c630e8540facad9
honghua <honghua@iscas.ac.cn>
2026-09-18 09:02:00 +0800

    test(acl): add filesystem ACL support diagnostics to chacl_command

--- tests/functional/pkgs/acl/test_acl_chacl_command/test.sh
+++ tests/functional/pkgs/acl/test_acl_chacl_command/test.sh
@@ -22,6 +22,18 @@ rlJournalStart
 
     TmpDir=$(mktemp -d)
 
+    # DEBUG: check if filesystem supports ACL
+    rlLog "DEBUG TmpDir=$TmpDir"
+    rlLog "DEBUG mount: $(mount | grep -E \"$(df $TmpDir | tail -1 | awk '{print $1}')\")"
+    rlLog "DEBUG filesystem type: $(df -T $TmpDir | tail -1)"
+
+    # DEBUG: quick setfacl + getfacl round-trip test (exit 0 ≠ ACL written)
+    touch "$TmpDir/_acl_test"
+    setfacl -m u::rwx "$TmpDir/_acl_test" 2>&1
+    rlLog "DEBUG ACL round-trip: $(getfacl "$TmpDir/_acl_test" 2>&1)"
+    # Check if filesystem mounted with acl option
+    rlLog "DEBUG /proc/mounts acl: $(grep -E \"$(df $TmpDir | tail -1 | awk '{print $1}')\" /proc/mounts 2>/dev/null)"
+
     rlRun "cd $TmpDir" 0 "Enter temporary test directory"
 
     rlRun "touch testfile" 0 "Create test file"
@@ -44,8 +56,10 @@ rlJournalStart
     # chacl basic set equivalent: use setfacl -m
     rlRun "setfacl -m u::rw-,g::r--,o::r-- testfile" 0 "use setfacl setbasic ACL"
 
-    output=$(getfacl testfile 2>&1)
+    # DEBUG: print actual getfacl output after setfacl
+    rlLog "DEBUG after setfacl basic: $(getfacl testfile 2>&1)"
 
+    output=$(getfacl testfile 2>&1)
     rlRun "echo \"\$output\" | grep -q 'user::rw-'" 0 "confirm chacl set user ACL"
 
 
@@ -54,6 +68,10 @@ rlJournalStart
     # recurse into sub-files on this system. Use setfacl with -R to
     # verify that ACLs can be applied recursively to directory contents.
     rlRun "setfacl -R -m u::rw-,g::r--,o::r-- testdir" 0 "use setfacl recursive to set ACL"
+
+    # DEBUG: print actual getfacl output after recursive setfacl
+    rlLog "DEBUG after setfacl -R: testdir=$(getfacl testdir 2>&1)"
+    rlLog "DEBUG after setfacl -R: testdir/file1=$(getfacl testdir/file1 2>&1)"
 
     output=$(getfacl testdir/file1 2>&1)
 
@@ -64,11 +82,19 @@ rlJournalStart
     # chacl -d (set default ACL) is not available on this Linux version;
     # use setfacl instead to verify default ACL functionality.
     rlRun "setfacl -m d:u::rwx,d:g::r-x,d:o::r-x testdir" 0 "set default ACL"
+
+    # DEBUG: print actual getfacl output after setting default ACL
+    rlLog "DEBUG after set default ACL: $(getfacl testdir 2>&1)"
+
     output=$(getfacl testdir 2>&1)
     rlRun "echo \"\$output\" | grep -q 'default:user::rwx'" 0 "confirm default ACL was set"
 
     # Verify both access and default ACL entries coexist
     rlRun "setfacl -m u::rwx,g::r-x,o::r-x testdir" 0 "set access ACL"
+
+    # DEBUG: print actual getfacl output after setting access ACL (coexistence check)
+    rlLog "DEBUG after set access ACL (coexistence): $(getfacl testdir 2>&1)"
+
     output=$(getfacl testdir 2>&1)
     rlRun "echo \"\$output\" | grep -q 'default:user::rwx'" 0 "confirm both access+default present"
 
